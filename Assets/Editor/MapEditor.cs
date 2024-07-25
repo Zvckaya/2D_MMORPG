37d4b89253e9dda56be51cc20fb6ab7d786f4418
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-25 21:29:30 +0900

    폴더reading 하여 map Collider 생성

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -21,35 +21,35 @@ public class MapEditor
 
     private static void GenerateMap()
     {
-        GameObject go = GameObject.Find("Map");
-        if (go == null)
-            return;
+        GameObject[] gameObject = Resources.LoadAll<GameObject>("Prefabs/Map");
 
-        Tilemap _tm= Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
-        if (_tm == null)
-            return;
-
-        using (var writer = File.CreateText("Assets/Resources/Map/output.txt"))
+        foreach (GameObject go in gameObject)
         {
-            writer.WriteLine(_tm.cellBounds.xMin);
-            writer.WriteLine(_tm.cellBounds.xMax);
-            writer.WriteLine(_tm.cellBounds.yMin);
-            writer.WriteLine(_tm.cellBounds.yMax);
+            Tilemap _tm = Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
 
-            for(int y= _tm.cellBounds.yMax; y >= _tm.cellBounds.yMin; y--)
+            using (var writer = File.CreateText($"Assets/Resources/Map/{go.name}.txt"))
             {
-                for(int x=_tm.cellBounds.xMin;x <= _tm.cellBounds.xMax; x++)
+                writer.WriteLine(_tm.cellBounds.xMin);
+                writer.WriteLine(_tm.cellBounds.xMax);
+                writer.WriteLine(_tm.cellBounds.yMin);
+                writer.WriteLine(_tm.cellBounds.yMax);
+
+                for (int y = _tm.cellBounds.yMax; y >= _tm.cellBounds.yMin; y--)
                 {
-                    TileBase tile= _tm.GetTile(new Vector3Int(x, y, 0));
-                    if (tile != null)
-                        writer.Write("1");
-                    else
-                        writer.Write("0");
+                    for (int x = _tm.cellBounds.xMin; x <= _tm.cellBounds.xMax; x++)
+                    {
+                        TileBase tile = _tm.GetTile(new Vector3Int(x, y, 0));
+                        if (tile != null)
+                            writer.Write("1");
+                        else
+                            writer.Write("0");
+                    }
+                    writer.WriteLine();
                 }
-                writer.WriteLine();
             }
         }
 
+  
     }
 
 #endif
