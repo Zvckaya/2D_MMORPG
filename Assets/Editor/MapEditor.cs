2e8b315ab4a3d9d3b6d93998f47ca1e3726666bd
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-25 21:22:49 +0900

    tilemap read해서 충돌배열 생성

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.IO;
+
+
+
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -10,19 +15,41 @@ public class MapEditor
 {
 #if UNITY_EDITOR
 
-    [MenuItem("Tools/GenerateMap")]
+   
 
-    private static void HelloWorld()
+    [MenuItem("Tools/GenerateMap %#g")]
+
+    private static void GenerateMap()
     {
-        
-        if (EditorUtility.DisplayDialog("Hello World", "Create?", "Create", "Cancel"))
-        {
-            new GameObject("Hello World");
-        }
-        else
-        {
+        GameObject go = GameObject.Find("Map");
+        if (go == null)
+            return;
 
+        Tilemap _tm= Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
+        if (_tm == null)
+            return;
+
+        using (var writer = File.CreateText("Assets/Resources/Map/output.txt"))
+        {
+            writer.WriteLine(_tm.cellBounds.xMin);
+            writer.WriteLine(_tm.cellBounds.xMax);
+            writer.WriteLine(_tm.cellBounds.yMin);
+            writer.WriteLine(_tm.cellBounds.yMax);
+
+            for(int y= _tm.cellBounds.yMax; y >= _tm.cellBounds.yMin; y--)
+            {
+                for(int x=_tm.cellBounds.xMin;x <= _tm.cellBounds.xMax; x++)
+                {
+                    TileBase tile= _tm.GetTile(new Vector3Int(x, y, 0));
+                    if (tile != null)
+                        writer.Write("1");
+                    else
+                        writer.Write("0");
+                }
+                writer.WriteLine();
+            }
         }
+
     }
 
 #endif
