0f221f9718cea9e2f578799e32d0f7f085fe60a3
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-25 21:48:55 +0900

    GetDirInput추가

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using static Define;
 
 public class PlayerController : MonoBehaviour
 {
     float _speed = 5.0f;
+
+    MoveDir _dir = MoveDir.None;
 
     // Start is called before the first frame update
     void Start()
@@ -15,26 +19,34 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
+    }
+
+    void GetDirInput()
+    {
         if (Input.GetKey(KeyCode.W))
         {
             //transform.position += Vector3.up * Time.deltaTime * _speed;
+            _dir = MoveDir.Up;
         }
         else if (Input.GetKey(KeyCode.S))
         {
             //transform.position += Vector3.down * Time.deltaTime * _speed;
-
+            _dir = MoveDir.Down;
         }
         else if (Input.GetKey(KeyCode.A))
         {
             //transform.position += Vector3.left * Time.deltaTime * _speed;
+            _dir = MoveDir.Left;
         }
-        else if(Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             //transform.position += Vector3.right * Time.deltaTime * _speed;
+            _dir = MoveDir.Right;
         }
         else
         {
-
+            _dir = MoveDir.None;
         }
     }
 }
