1de1edc454b160f1943b2c3bc8696d96477b6239
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-26 20:36:23 +0900

    mapeditor 수정

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -25,18 +25,19 @@ public class MapEditor
 
         foreach (GameObject go in gameObject)
         {
+            Tilemap tmBase = Util.FindChild<Tilemap>(go, "Tilemap_Base", true);
             Tilemap _tm = Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
 
             using (var writer = File.CreateText($"Assets/Resources/Map/{go.name}.txt"))
             {
-                writer.WriteLine(_tm.cellBounds.xMin);
-                writer.WriteLine(_tm.cellBounds.xMax);
-                writer.WriteLine(_tm.cellBounds.yMin);
-                writer.WriteLine(_tm.cellBounds.yMax);
+                writer.WriteLine(tmBase.cellBounds.xMin);
+                writer.WriteLine(tmBase.cellBounds.xMax);
+                writer.WriteLine(tmBase.cellBounds.yMin);
+                writer.WriteLine(tmBase.cellBounds.yMax);
 
-                for (int y = _tm.cellBounds.yMax; y >= _tm.cellBounds.yMin; y--)
+                for (int y = tmBase.cellBounds.yMax; y >= tmBase.cellBounds.yMin; y--)
                 {
-                    for (int x = _tm.cellBounds.xMin; x <= _tm.cellBounds.xMax; x++)
+                    for (int x = tmBase.cellBounds.xMin; x <= tmBase.cellBounds.xMax; x++)
                     {
                         TileBase tile = _tm.GetTile(new Vector3Int(x, y, 0));
                         if (tile != null)
