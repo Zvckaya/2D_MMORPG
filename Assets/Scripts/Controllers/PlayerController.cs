09b7c2e7f309adbf23f0a3d474b1cab148b3baf9
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-26 21:53:02 +0900

    PlayerController 수정

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -5,85 +5,19 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using static Define;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : CreatureController
 {
     
-    float _speed = 5.0f;
-
-    Vector3Int _cellPos = Vector3Int.zero;
-    MoveDir _dir = MoveDir.Down;
-    bool _isMoving = false;
-    Animator _animator;
-
-
-    public MoveDir Dir
+   
+    protected override void Init()
     {
-        get { return _dir; }
-        set {
-            if (_dir == value)
-                return;
-
-            switch (value)
-            {
-                case MoveDir.Up:
-                    _animator.Play("WALK_DOWN");
-                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-                    break;
-                case MoveDir.Down:
-                    _animator.Play("WALK_UP");
-                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-                    break;
-                case MoveDir.Left:
-                    _animator.Play("WALK_RIGHT");
-                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-                    break;
-                case MoveDir.Right:
-                    _animator.Play("WALK_RIGHT");
-                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-                    break;
-                case MoveDir.None:
-                    if (_dir == MoveDir.Up)
-                    {
-                        _animator.Play("IDLE_DOWN");
-                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-                    }
-                    else if (_dir == MoveDir.Down) {
-                        _animator.Play("IDLE_UP");
-                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-                    }
-                    else if (_dir == MoveDir.Left)
-                    {
-                        _animator.Play("IDLE_RIGHT");
-                        transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-                    }
-                    else
-                    {
-                        _animator.Play("IDLE_RIGHT");
-                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-                    }
-                    break;
-            }
-            _dir = value;
-        }
+        base.Init();
     }
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _animator = GetComponent<Animator>();
-        Vector3 pos = Managers.Map.CurrentGrid.CellToWorld(_cellPos)+ new Vector3(0.5f,0.5f);
-        transform.position = pos;
-
-    }
-
-    // Update is called once per frame
-    void Update()
+    protected override void UpdateController()
     {
         GetDirInput();
-        UpdatePosition();
-        UpdateIsMoving();
-        
+        base.UpdateController();
     }
 
     private void LateUpdate()
@@ -92,60 +26,7 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    //이동가능한 상태일때 실제로 이동 
-    void UpdatePosition()
-    {
-        if (_isMoving == false)
-            return;
-
-        Vector3 destPos = Managers.Map.CurrentGrid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f); //서버위치
-        Vector3 moveDir = destPos - transform.position; 
-
-        float dist = moveDir.magnitude; //방향벡터의 크기, 즉 얼마나 남았는가?
-        if(dist< _speed * Time.deltaTime)  //사실상 도착했는가? 
-        {
-            transform.position = destPos;
-            _isMoving = false;
-        }
-        else
-        {
-            transform.position += moveDir.normalized * _speed * Time.deltaTime; //너무 스피드가 빠르면 그 이상 갈 수도 있음 
-            _isMoving = true;
-        }
-         
-    }
-
-
-    //실질적 이동 
-    void UpdateIsMoving()
-    {
-        if (_isMoving == false &&_dir != MoveDir.None)
-        {
-            Vector3Int destPos = _cellPos;
-            switch (_dir)
-            {
-                case MoveDir.Up:
-                    destPos += Vector3Int.up;
-                    break;
-                case MoveDir.Down:  
-                    destPos += Vector3Int.down;
-                    break;
-                case MoveDir.Left:
-                    destPos += Vector3Int.left;
-                    break;
-                case MoveDir.Right:
-                    destPos += Vector3Int.right;
-                    break;
-            }
-
-            if (Managers.Map.CanGo(destPos))
-            {
-                _cellPos = destPos;
-                Debug.Log($"player move{destPos.ToString()}");
-                _isMoving = true;
-            }
-        }
-    }
+   
 
     //키보드 입력을 받아 방향 설정 
     void GetDirInput()
